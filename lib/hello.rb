fa39cf79358f3a12d4b560741acdc53eebbc8bae
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 16:40:33 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,15 @@
+def hello_t(array)
+  if block_given?
+    i = 0
 
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+  else
+    puts "Hey! No block was given!"
+  end
+end
 
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
