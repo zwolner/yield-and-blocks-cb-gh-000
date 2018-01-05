327b22a69e3c6117a57a0b921caa2c390b8f6238
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 16:43:27 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,6 +8,7 @@ def hello_t(array)
   else
     puts "Hey! No block was given!"
   end
+  array
 end
 
 # call your method here!
@@ -15,5 +16,4 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
-  array
 end
